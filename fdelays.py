2a56d68306f3ad018be7f6fdf82e75dd18a7e2ba
simar1894 <simardeep1894@gmail.com>
2019-05-26 18:18:32 -0700

    added names in files

--- fdelays.py
+++ fdelays.py
@@ -3,3 +3,4 @@ import pandas as pd
 df= pd.read_csv("flightdelays.csv.1")
 print(df[(df["Origin"]=="SFO")]["ArrDelay"].head(3))
 print(df["Dest"].value_counts().head(3))
+print("Simardeep")
--- flightdelay.sh
+++ flightdelay.sh
@@ -1,3 +1,4 @@
 # !/usr/bin/env bash
 cut -d"," -f 15,17 flightdelays.csv.2 | grep "SFO" |head -3 | cut -d"," -f 1 > first3sfo.csv
 cat first3sfo.csv
+echo "Simardeep"
